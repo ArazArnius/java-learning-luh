26e9da9413a7778e1dcbffaa55a80a006105de90
Araz Arnius <araz.arnius@gmail.com>
2025-04-18 16:52:49 +0200

    task 1-c) done but without using binary search

--- Assignment02/task1/model/Tree.java
+++ Assignment02/task1/model/Tree.java
@@ -46,26 +46,6 @@ public class Tree {
         return in_order;
     }
 
-    // checks whether a value is in the tree and uses binary tree features
-    // is in O(log n)
-    public boolean contains (int value){
-        boolean con = false;
-        if (this.value == value){
-            return true;
-        }
-
-        if (left != null && con == false){
-            con = left.contains(value);
-        }
-
-        if (right != null && con == false){
-            con = right.contains(value);
-        }
-
-        return con;
-    }
-
-    /*
     // checks whether the node `value` is in the tree
     // but is O(n) since it doesn't really use the binary tree features
     public boolean contains (int value){
@@ -84,7 +64,7 @@ public class Tree {
 
         return con;
     }
-    */
+
 
     public static void main(String[] args){
         Tree node1 = new Tree(5);

c4ae3b1f48c879402bd3b95407e42350149e1c1a
Araz Arnius <araz.arnius@gmail.com>
2025-04-18 15:34:04 +0200

    variable names changed

--- Assignment02/task1/Main.java
+++ Assignment02/task1/Main.java
@@ -3,17 +3,17 @@ import model.*;
 public class Main {
     public static void main (String[] args){
         // Test case 1:
-        Tree nodea1 = new Tree(5);
-        Tree nodea2 = new Tree(3);
-        Tree nodea3 = new Tree(8);
-        Tree nodea4 = new Tree(1);
-        Tree nodea5 = new Tree(4);
+        Tree node_a1 = new Tree(5);
+        Tree node_a2 = new Tree(3);
+        Tree node_a3 = new Tree(8);
+        Tree node_a4 = new Tree(1);
+        Tree node_a5 = new Tree(4);
 
-        nodea1.setLeft(nodea2);
-        nodea1.setRight(nodea3);
+        node_a1.setLeft(node_a2);
+        node_a1.setRight(node_a3);
 
-        nodea2.setLeft(nodea4);
-        nodea2.setRight(nodea5);
+        node_a2.setLeft(node_a4);
+        node_a2.setRight(node_a5);
 
         /* Tree in question:
             5
@@ -23,20 +23,20 @@ public class Main {
         1   4
         */
 
-        System.out.println(nodea1.str()); // Expected: 1 3 4 5 8
+        System.out.println(node_a1.str()); // Expected: 1 3 4 5 8
 
         // Test case 2
-        Tree nodeb1 = new Tree(5);
-        Tree nodeb2 = new Tree(3);
-        Tree nodeb3 = new Tree(8);
-        Tree nodeb4 = new Tree(7);
-        Tree nodeb5 = new Tree(12);
+        Tree node_b1 = new Tree(5);
+        Tree node_b2 = new Tree(3);
+        Tree node_b3 = new Tree(8);
+        Tree node_b4 = new Tree(7);
+        Tree node_b5 = new Tree(12);
 
-        nodeb1.setLeft(nodeb2);
-        nodeb1.setRight(nodeb3);
+        node_b1.setLeft(node_b2);
+        node_b1.setRight(node_b3);
 
-        nodeb3.setLeft(nodeb4);
-        nodeb3.setRight(nodeb5);
+        node_b3.setLeft(node_b4);
+        node_b3.setRight(node_b5);
 
         /* Tree in question:
             5
@@ -46,6 +46,6 @@ public class Main {
             7   12
         */
 
-        System.out.println(nodeb1.str()); // Expected: 3 5 7 8 12
+        System.out.println(node_b1.str()); // Expected: 3 5 7 8 12
     }
 }

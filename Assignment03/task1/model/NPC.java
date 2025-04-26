658e5595590a8295817cb1b25f6244cc08fabe1f
Araz Arnius <araz.arnius@gmail.com>
2025-04-26 15:03:31 +0200

    NPC implemented

---
+++ Assignment03/task1/model/NPC.java
@@ -0,0 +1,68 @@
+package model;
+
+/**
+ * represents the NPC (non playable character) that is gonna be moving in the game.
+ * the NPC can either be standing or crouching
+ */
+public class NPC {
+    /** represents the x-coordinate of the NPC */
+    private int x_coordinate;
+    /** indicates whether the NPC is crouching or standing */
+    private boolean is_crouching;
+
+    public NPC(x_coordinate, is_crouching) {
+        this.x_coordinate = x_coordinate;
+        this.is_crouching = is_crouching;
+    }
+    /** getter method for {@link model.NPC#x_coordinate} attribute */
+    public int getXCoordinate() {
+        return x_coordinate;
+    }
+
+    /** setter method for {@link model.NPC#x_coordinate} attribute
+     * {@link model.NPC#x_coordinate} represents the x-coordinate of the NPC
+     * 
+     * @param x_coordinate
+     */
+    public void setXCoordinate(x_coordinate) {
+        this.x_coordinate = x_coordinate;
+    }
+
+    /** getter method for {@link model.NPC#is_crouch} attribute
+     * {@link model.NPC#is_crouch} indicates whether the NPC is crouching or standing
+     * 
+     * @return is_crouching
+     */
+    public int getIsCrouch() {
+        return is_crouching;
+    }
+
+    /** alter method for {@link model.NPC#is_crouch} attribute to make NPC stand */
+    public void stand() {
+        is_crouching = false;
+    }
+
+    /** alter method for {@link model.NPC#is_crouch} attribute to make NPC crouch */
+    public void crouch() {
+        is_crouching = true;
+    }
+
+    /** method to move the NPC one unit to the right
+     * increases {@link model.NPC#x_coordinate} by one
+     */
+    public void walkRight() {
+        x_coordinate += 1;
+    }
+
+    /** method for the NPC to dash (move to right by 2 units)
+     * dashing is only possible if {@link model.NPC#is_crouching} is false (the NPC is standing)
+     * if the character is crouching then nothing happens when this method is called
+     */
+    public void dashRight() {
+        if (getIsCrouch) {
+            return;
+        }
+
+        x_coordinate += 2;
+    }
+}

7909e23848f3836acef0d4037715c1bd770bf604
Araz Arnius <araz.arnius@gmail.com>
2025-05-12 18:42:27 +0200

    optimized the code

--- Minitestat/1/Triangle.java
+++ Minitestat/1/Triangle.java
@@ -1,7 +1,7 @@
 public class Triangle {
     public static void main (String[] args) {
         int len = Integer.parseInt(args[0]);
-        String result = "";
+        StringBuilder result = new StringBuilder(); // ChatGpt doesn't like the string concatenation in the loops
         System.out.println("The given length of the pyramid is " + len + "\n");
 
         /*
@@ -15,12 +15,12 @@ public class Triangle {
         for (int i = 1; i < len + 1; i++) {
             for (int j = 0; j < len; j++) { // spaces on the beginning
                 if (j < len - i) {
-                    result += " ";
+                    result.append(" ");
                 } else { // then asterisks
-                    result += "*";
+                    result.append("*");
                 }
             }
-            result += "\n";
+            result.append("\n");
         }
 
         System.out.println(result);

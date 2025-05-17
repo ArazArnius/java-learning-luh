2b2504ea1c705652c0209a4aa482d4f92b565162
Araz Arnius <araz.arnius@gmail.com>
2025-05-17 22:03:53 +0200

    add enum Action

---
+++ Assignment05/task1/src/treasurehunt/actions/Actions.java
@@ -0,0 +1,16 @@
+package actions;
+
+public enum Action {
+    A('A'),
+    B('B');
+
+    final char action;
+
+    Action(char action) {
+        this.action = action;
+    }
+
+    public char str() {
+        return action;
+    }
+}

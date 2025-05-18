40a15d3a3ee9de8a057b9411deb3658a0ad5d0ed
Araz Arnius <araz.arnius@gmail.com>
2025-05-18 13:39:57 +0200

    add JavaDoc and small fixes

--- Assignment05/task1/src/treasurehunt/states/State.java
+++ Assignment05/task1/src/treasurehunt/states/State.java
@@ -1,16 +1,29 @@
 package states;
-Import actions.Action;
+import actions.Action;
 
-// package private access
-interface State {
-    // no need to explicitly declare methods public as all methods in an interface are public and abstract by default
-
-    // performs the state transition based on the current state and the given action. It returns the next state.
+/**
+ * Represents a state in the finite deterministic automaton.
+ */
+public interface State {
+    /**
+     * Performs the state transition based on the current state and the given action.
+     *
+     * @param action the action taken
+     * @return the next state after transition
+     */
     State transition(Action action);
 
-    // returns a description of the current state and the possible transitions (e.g., “Action A → Shipwreck Bay; Action B → Musket Hill”).
+    /**
+     * Returns just the name of the state.
+     *
+     * @return the state name
+     */
     String str();
 
-    // returns just the name of the state.
+    /**
+     * Returns a description of the state and possible transitions.
+     *
+     * @return information about the state
+     */
     String info();
 }

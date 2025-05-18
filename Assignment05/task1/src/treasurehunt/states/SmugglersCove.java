d3e79277da500de9027a416480367b58ddb8b34c
Araz Arnius <araz.arnius@gmail.com>
2025-05-18 17:17:21 +0200

    add SmugglersCove state

---
+++ Assignment05/task1/src/treasurehunt/states/SmugglersCove.java
@@ -0,0 +1,51 @@
+package treasurehunt.states;
+import treasurehunt.actions.Action;
+
+/**
+ * Represents the state Smugglers' Cove in the automaton.
+ */
+public class SmugglersCove implements State {
+    /**
+     * Performs the state transition based on the current state and the given action.
+     *
+     * @param action the action taken
+     * @return the next state after transition
+     */
+    @Override
+    public State transition(Action action) {
+        return action == Action.A ? new PiratesIsland() : new TreasureIsland();
+    }
+
+    /**
+     * Returns just the name of the state.
+     *
+     * @return the state name
+     */
+    @Override
+    public String str() {
+        return "Smugglers' Cove";
+    }
+
+    /**
+     * Returns a description of the state and possible transitions.
+     *
+     * @return information about the state
+     */
+    @Override
+    public String info() {
+        return str() + 
+            "\nAction A → " + transition(Action.A).str() +
+            "; Action B → " + transition(Action.B).str();
+    }
+
+    /**
+     * Test method for SmugglersCove state.
+     */
+    public static void main(String[] args) {
+        SmugglersCove obj = new SmugglersCove();
+        System.out.println("Current state: " + obj.str());
+        System.out.println("If action A: " + obj.transition(Action.A).str());
+        System.out.println("If action B: " + obj.transition(Action.B).str());
+        System.out.println("All possible actions from " + obj.info());
+    }
+}

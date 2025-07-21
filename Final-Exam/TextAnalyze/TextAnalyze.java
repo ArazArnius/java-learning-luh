f30d5e8fb0a06ffbd66e35341c196746950adf09
Araz Arnius <araz.arnius@gmail.com>
2025-07-21 16:31:16 +0200

    Text Analyze

--- Final-Exam/TextAnalyze/TextAnalyze.java
+++ Final-Exam/TextAnalyze/TextAnalyze.java
@@ -3,39 +3,38 @@ import java.util.*;
 
 public class TextAnalyze {
     public static void main(String[] args) {
-        String text = "Dieser Text dient als Beispieleingabe für die obige Aufgabe";
-        String[] words = text.split(" +");
-        // ArrayList<String> wordsList = new ArrayList<>();
-        ArrayList<Integer> lenList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        String mode = "y";
+        while (mode.equals("y")) {
+            System.out.print("Please enter your text:\t");
+            String text = sc.nextLine();
+            String[] words = text.split(" +");
+            ArrayList<Integer> lenList = new ArrayList<>();
 
-        for (String word : words) {
-            // wordsList.add(word);
-            int len = word.length();
-            if (!lenList.contains(len)) {
-                lenList.add(len);
-            }
-        } // not needed
-
-        System.out.println("unsorted:\n" + text);
-        // for (String word : wordsList) {
-        //     System.out.print(word + " ");
-        // } // not needed
-
-        // System.out.println("lenghts:");
-        // for (int l : lenList) {
-        //     System.out.print(l + " ");
-        // } // works
-
-        Collections.sort(lenList);
-
-        for (int l : lenList) {
-            System.out.print(l + ": ");
             for (String word : words) {
-                if (word.length() == l) {
-                    System.out.print(word + " ");
+                int len = word.length();
+                if (!lenList.contains(len)) {
+                    lenList.add(len);
                 }
+            } 
+
+            System.out.println("Entered text:\n" + text);
+
+            Collections.sort(lenList);
+
+            for (int l : lenList) {
+                System.out.print(l + ": ");
+                for (String word : words) {
+                    if (word.length() == l) {
+                        System.out.print(word + " ");
+                    }
+                }
+                System.out.println();
             }
-            System.out.println();
+
+            System.out.print("Do you want to continue? (y/n)\t");
+            mode = sc.nextLine();
         }
+        System.out.println("Bye bye");
     }
 }
